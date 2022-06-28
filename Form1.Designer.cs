47a8909701f6f62678b5468e61d7364023cb8e91
Student <Student@MERCY>
2022-06-28 13:19:03 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace TemperatureAverage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button3 = new System.Windows.Forms.Button();
+            this.averageButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(420, 300);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 39);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "EXIT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // averageButton
+            // 
+            this.averageButton.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.averageButton.Location = new System.Drawing.Point(179, 300);
+            this.averageButton.Name = "averageButton";
+            this.averageButton.Size = new System.Drawing.Size(150, 39);
+            this.averageButton.TabIndex = 5;
+            this.averageButton.Text = "AVERAGE";
+            this.averageButton.UseVisualStyleBackColor = true;
+            this.averageButton.Click += new System.EventHandler(this.averageButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(212, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "ENTER TEMPERATURE IN FAHRENHEIT:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(457, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(90, 94);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            130,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 15;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(90, 132);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            130,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 16;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(90, 167);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            130,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 17;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(90, 205);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            130,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown4.TabIndex = 18;
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(90, 242);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            130,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Minimum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown5.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 26);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "The temperature must range between 130 and -30\r\nIf not entered correctly the valu" +
+    "e will be replaced with a valid value.\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 377);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.averageButton);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button averageButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemperatureAverage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //EXIT BUTTON EVENT
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        //AVERAGE BUTTON EVENT (MAIN FUNCTION)
+        private void averageButton_Click(object sender, EventArgs e)
+        {
+            decimal COUNTER = 5;
+            decimal averageTemperature;
+
+            decimal doMonday = numericUpDown1.Value;
+            decimal doTuesday = numericUpDown2.Value;
+            decimal doWednesday = numericUpDown3.Value;
+            decimal doThursday = numericUpDown4.Value;
+            decimal doFriday = numericUpDown5.Value;
+
+            decimal total = doMonday + doThursday + doTuesday + doWednesday + doFriday;
+
+            averageTemperature = total / COUNTER;
+
+            if (doMonday <= doTuesday && doTuesday <= doWednesday && doWednesday <= doThursday && doThursday <= doFriday)
+            {
+                MessageBox.Show("Getting warmer :" + Convert.ToString(averageTemperature));
+            }
+            else if (doMonday >= doTuesday && doTuesday >= doWednesday && doWednesday >= doThursday && doThursday >= doFriday)
+            {
+                MessageBox.Show("Getting cooler :" + Convert.ToString(averageTemperature));
+            }
+            else
+            {
+                MessageBox.Show("It's a mixed bag :" + Convert.ToString(averageTemperature));
+            }
+
+        }
+        // I WAS TRYING TO RESTRICT USER INPUT EVENT - I ADDED REFERENCES, BUT IT DID NOT WORK
+        /*private void numericUpDown1_ValueChanged(Object sender, EventArgs e)
+        {
+
+            if (numericUpDown1.Value >= 130 || numericUpDown1.Value <= -30)
+            {
+                MessageBox.Show("You enter an invalid value, must be between -30 and 130");
+            }
+
+        }
+        
+        private void numericUpDown2_ValueChanged(Object sender, EventArgs e)
+        {
+            decimal doMonday = numericUpDown1.Value;
+
+            if (doMonday >= 130 || doMonday <= -30)
+            {
+                MessageBox.Show("You enter an invalid value, must be between -30 and 130");
+            }
+
+        }
+
+        private void numericUpDown3_ValueChanged(Object sender, EventArgs e)
+        {
+            decimal doMonday = numericUpDown1.Value;
+
+            if (doMonday >= 130 || doMonday <= -30)
+            {
+                MessageBox.Show("You enter an invalid value, must be between -30 and 130");
+            }
+
+        }
+        private void numericUpDown4_ValueChanged(Object sender, EventArgs e)
+        {
+            decimal doMonday = numericUpDown1.Value;
+
+            if (doMonday >= 130 || doMonday <= -30)
+            {
+                MessageBox.Show("You enter an invalid value, must be between -30 and 130");
+            }
+
+        }
+
+        private void numericUpDown5_ValueChanged(Object sender, EventArgs e)
+        {
+            decimal doMonday = numericUpDown1.Value;
+
+            if (doMonday >= 130 || doMonday <= -30)
+            {
+                MessageBox.Show("You enter an invalid value, must be between -30 and 130");
+            }
+
+        }*/
+
+    }
+}
+
+
